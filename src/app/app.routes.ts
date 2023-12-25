2f252fb2b7596dd30a319e1186526afb23b7d035
eyder-suarez <ey_der19@hotmail.com>
2023-12-25 14:30:08 -0500

    Se comienza a crear el detalle de cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { DetallesClienteComponent } from './pages/detalles-cliente/detalles-cliente.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/clientes' },
-  { path: 'clientes', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+  { path: 'clientes', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: 'id', component: DetallesClienteComponent }
 ];
