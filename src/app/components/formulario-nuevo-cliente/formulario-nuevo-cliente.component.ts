af7d19b016f9afa7ac5a76091538476b3a011a70
Eyder Suarez <ey_der19@hotmail.com>
2024-02-27 22:00:13 -0500

    Se agregan las validaciones del formulario de registro del cliente

--- src/app/components/formulario-nuevo-cliente/formulario-nuevo-cliente.component.ts
+++ src/app/components/formulario-nuevo-cliente/formulario-nuevo-cliente.component.ts
@@ -4,13 +4,14 @@ import { ValidatorPlateService } from '../../services/plate.service';
 import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
 import { DatosClientesService } from '../../services/datos-clientes.service';
 import { BRANDS } from '../../interfaces/marcas-vehiculos';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-formulario-nuevo-cliente',
   standalone: true,
-  imports: [NgZorroModule, ReactiveFormsModule, FormsModule,],
+  imports: [NgZorroModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './formulario-nuevo-cliente.component.html',
   styleUrl: './formulario-nuevo-cliente.component.css'
 })
@@ -26,7 +27,8 @@ export class FormularioNuevoClienteComponent implements OnInit {
   private fb                    = inject(FormBuilder);
   private validatorPlateService = inject(ValidatorPlateService);
   private dataService           = inject(DatosClientesService);
-  private router                = inject(Router)
+  private router                = inject(Router);
+  private activatedRoute        = inject(ActivatedRoute);
 
   ngOnInit() {
 
@@ -37,6 +39,7 @@ export class FormularioNuevoClienteComponent implements OnInit {
     this.dataService.getBrandVehicles().subscribe((marcas: any) => {
       this.brands = marcas
     })
+    this.activatedRoute.params.subscribe(({ id }) => this.id = id) // Tomar el ID del cliente
 
     this.formNuevoCliente = this.fb.group({
       name           : ['', [Validators.required, Validators.maxLength(50), Validators.minLength(5)]],
@@ -48,6 +51,16 @@ export class FormularioNuevoClienteComponent implements OnInit {
       vehicleBrand   : ['', [Validators.required]],
       plate          : [''.toUpperCase(), [Validators.required, Validators.minLength(6), Validators.maxLength(6)], [this.validatorPlateService ]]
     })
+  }
+
+   /**
+   *
+   * Para validar los campos del formulario
+   * @returns
+   */
+   invalidField( field: string ) {
+    return this.formNuevoCliente.get(field)?.invalid
+            && this.formNuevoCliente.get(field)?.touched;
   }
 
 
@@ -77,7 +90,7 @@ export class FormularioNuevoClienteComponent implements OnInit {
        this.formNuevoCliente.get('plate')?.disable
        let editForm = this.formNuevoCliente.value
        this.dataService.editClientDocument(this.id, editForm).subscribe();
-       //!Debe ir la notificacion de notificacion cliente
+       //!Debe ir la notificacion de edicion datos del cliente
        setTimeout(() => {
          this.router.navigate([this.id]);
        },2000)
