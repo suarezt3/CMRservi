42cdbd8ebd5db8c1bcfc8f1ccbbe8b56d2f8e4ea
Eyder Suarez <ey_der19@hotmail.com>
2024-03-14 11:07:14 -0500

    Se ajusta la tabla de trabajos

--- src/app/components/tabla-trabajos/tabla-trabajos.component.ts
+++ src/app/components/tabla-trabajos/tabla-trabajos.component.ts
@@ -3,11 +3,12 @@ import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
 import { DatosClientesService } from '../../services/datos-clientes.service';
 import { Trabajo } from '../../interfaces/trabajos';
 import { CommonModule } from '@angular/common';
+import { SearchPlacaComponent } from '../search-placa/search-placa.component';
 
 @Component({
   selector: 'app-tabla-trabajos',
   standalone: true,
-  imports: [NgZorroModule, CommonModule,],
+  imports: [NgZorroModule, CommonModule],
   templateUrl: './tabla-trabajos.component.html',
   styleUrl: './tabla-trabajos.component.css'
 })
