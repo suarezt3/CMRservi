8b3b198fe643a08f551511af90c618c88c65274f
Eyder Suarez <ey_der19@hotmail.com>
2024-07-30 21:16:13 -0500

    Se activa el boton de nuevo trabajo en detalles del cliente

--- src/app/components/historial-trabajos-cliente/historial-trabajos-cliente.component.ts
+++ src/app/components/historial-trabajos-cliente/historial-trabajos-cliente.component.ts
@@ -1,21 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
 import { CommonModule } from '@angular/common';
+import { FormularioNuevoTrabajoComponent } from '../formulario-nuevo-trabajo/formulario-nuevo-trabajo.component';
+import { DatosClientesService } from '../../services/datos-clientes.service';
 
 @Component({
   selector: 'app-historial-trabajos-cliente',
   standalone: true,
-  imports: [NgZorroModule, CommonModule],
+  imports: [NgZorroModule, CommonModule, FormularioNuevoTrabajoComponent],
   templateUrl: './historial-trabajos-cliente.component.html',
   styleUrl: './historial-trabajos-cliente.component.css'
 })
 export class HistorialTrabajosClienteComponent implements OnInit {
 
   @Input() trabajosCliente!: any;
-  public textDate: string = "Siguiente "
+  @Input() cliente: any = "" || undefined;
+  public textDate: string = "Siguiente ";
+  public modalVisible : boolean   = false
+  public isOkLoading  : boolean   = false;
+  public visible      : boolean   = false
+
+  private DatosClientesService = inject( DatosClientesService );
 
   ngOnInit(): void {
 
+    setTimeout(() => {
+      console.log("CLIENTE", this.cliente);
+    }, 1000);
   }
+
+
+
+
+    /**
+    * Modal que carga el formulario para agregar los trabajos
+    */
+    mostrarModal(placa: any): void {
+     this.DatosClientesService.getClientPlate(placa).subscribe((resp: any) => {
+       this.cliente = resp
+       console.log("CLIENTE", this.cliente);
+     })
+     this.modalVisible = true;
+   }
+
+
+  cancelarModal(): void {
+    this.modalVisible = false;
+  }
+
 
 }
