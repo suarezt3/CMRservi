d8d5cf6628c07334bd09e6b30ae97c2e508b67e2
Eyder Suarez <ey_der19@hotmail.com>
2024-09-09 15:49:24 -0500

    Se solucionan errores de la libreria de graficos ngx-chart

--- src/app/pages/estadisticas/estadisticas.component.ts
+++ src/app/pages/estadisticas/estadisticas.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from '../../../assets/dumy';
 
 @Component({
   selector: 'app-estadisticas',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './estadisticas.component.html',
   styleUrl: './estadisticas.component.css'
 })
 export class EstadisticasComponent {
+
+  public single!: any[];
+  public multi!: any[];
+
+  public view: any = [700, 400];
+
+   // options
+   showXAxis = true;
+   showYAxis = true;
+   gradient = false;
+   showLegend = true;
+   showXAxisLabel = true;
+   xAxisLabel = 'Country';
+   showYAxisLabel = true;
+   yAxisLabel = 'Population';
+
+   colorScheme: any = {
+     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+   };
+
+   constructor() {
+    Object.assign(this, { single })
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
 
 }
