27c54cd94514f3f3fb399c3c5131b442c8e228e2
Eyder Suarez <ey_der19@hotmail.com>
2024-09-09 14:58:21 -0500

    Se agrega la ruta de estadisticas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DetallesClienteComponent } from './pages/detalles-cliente/detalles-cliente.component';
 import { NuevoClienteComponent } from './pages/nuevo-cliente/nuevo-cliente.component';
 import { TrabajosComponent } from './pages/trabajos/trabajos.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/clientes' },
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'clientes/:id', component: DetallesClienteComponent },
   { path: 'nuevo-cliente', component: NuevoClienteComponent },
   { path: 'editar/:id', component: NuevoClienteComponent },
-  { path: 'trabajos', component: TrabajosComponent}
+  { path: 'trabajos', component: TrabajosComponent},
+  { path: 'estadisticas', component: EstadisticasComponent }
 ];
