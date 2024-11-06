ada37970e7a53f364abb5adf550a32e9c72f7940
Eyder Suarez <ey_der19@hotmail.com>
2024-11-06 16:36:32 -0500

    Form de login

--- src/app/components/form-login/form-login.component.ts
+++ src/app/components/form-login/form-login.component.ts
@@ -1,18 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-login',
   standalone: true,
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.css'],
-  imports: [ NgZorroModule ]
+  imports: [ NgZorroModule, ReactiveFormsModule, CommonModule ]
 })
 export class FormLoginComponent implements OnInit {
+
+  private fb = inject(FormBuilder);
+  public formLogin!: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.formLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+
+   /**
+   *
+   * Para validar los campos del formulario
+   * @returns
+   */
+   invalidField( field: string ) {
+    return this.formLogin.get(field)?.invalid
+            && this.formLogin.get(field)?.touched;
+  }
+
+
+  submitForm() {
+    console.log("Formulario", this.formLogin.value);
   }
 
 }
